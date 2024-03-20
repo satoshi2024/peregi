2c9f1be83df4b3ec62bb0c8156d18da1c804e266
satoshi2024 <satoshikai.cho@gmail.com>
2024-03-20 13:07:03 +0900

    .

--- src/main/java/com/peregi/common/JacksonObjectMapper.java
+++ src/main/java/com/peregi/common/JacksonObjectMapper.java
@@ -18,9 +18,9 @@ import java.time.format.DateTimeFormatter;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 /**
- * 对象映射器:基于jackson将Java对象转为json，或者将json转为Java对象
- * 将JSON解析为Java对象的过程称为 [从JSON反序列化Java对象]
- * 从Java对象生成JSON的过程称为 [序列化Java对象到JSON]
+ * オブジェクト・マッパー：ジャックソンに基づき、Javaオブジェクトをjsonに、またはjsonをJavaオブジェクトにする。
+ *  JSONをJavaオブジェクトにパースするプロセスは、[JSONからJavaオブジェクトをデシリアライズする]と呼ばれます。
+ *  JavaオブジェクトからJSONを生成する処理は、[JavaオブジェクトをJSONにシリアライズ]と呼ばれる。
  */
 public class JacksonObjectMapper extends ObjectMapper {
 
@@ -30,10 +30,10 @@ public class JacksonObjectMapper extends ObjectMapper {
 
     public JacksonObjectMapper() {
         super();
-        //收到未知属性时不报异常
+        //未知の属性を受信しても例外を報告しない
         this.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        //反序列化时，属性不存在的兼容处理
+        //デシリアライズ時の存在しない属性の互換性のある処理
         this.getDeserializationConfig().withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
 
@@ -48,7 +48,7 @@ public class JacksonObjectMapper extends ObjectMapper {
                 .addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)))
                 .addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT)));
 
-        //注册功能模块 例如，可以添加自定义序列化器和反序列化器
+        //関数モジュールの登録 例えば、カスタムのシリアライザーとデシリアライザーを追加することができます
         this.registerModule(simpleModule);
     }
 }
--- src/main/java/com/peregi/config/WebMvcConfig.java
+++ src/main/java/com/peregi/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.peregi.config;
 
+import com.peregi.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -17,5 +22,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("静的なリソースマッピングを開始します...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * メッセージ・コンバーターによるmvcフレームワークの拡張
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("拡張メッセージコンバータ...");
+        //メッセージコンバータオブジェクトの作成
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //Javaオブジェクトをjsonに変換するJacksonの基本的な用途であるオブジェクト・コンバータを設定
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //上記のメッセージ・コンバーター・オブジェクトをmvcフレームワークのコンバーター・コレクションに追加
+        converters.add(0,messageConverter);
     }
 }
